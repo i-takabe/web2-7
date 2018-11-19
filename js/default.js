759e345187050c8dfe1b4e9a5c692ff6db949cc4
i-takabe <38421033+i-takabe@users.noreply.github.com>
2018-11-19 18:21:12 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -1,3 +1,23 @@
-document.getElementById('form').select.onchange = function(){
-    location.href = document.getElementById('form').select.value;
+var number = Math.floor(Math.random()*3);
+var loop = true;
+
+var message;
+while(loop){
+  var answer = parseInt(window.prompt('数あてゲィム'));
+
+  if(answer === number){
+    message = 'ATARI';
+    loop = false;
+  }
+  else if (answer < number) {
+    message = 'oops, more Big';
+  }
+  else if(answer > number){
+    message = 'oh, more Small';
+  }
+  else {
+    message = 'hey, please enter number';
+  }
+
+  document.getElementById('choice').textContent = message;
 }
